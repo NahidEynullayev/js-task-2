77337227cf58a32fcb63b4167e919e7dc67e0c34
NahidEynullayev <eynullayevnahid18@gmail.com>
2024-08-02 00:11:33 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+//? task 1
+
+//todo while ile
+
+// let a = 1;
+
+// while (a <= 5) {
+//   console.log(a);
+//   a++;
+
+// todo for ile
+
+// for (let index = 1; index <= 5; index++) {
+//     console.log(index);
+//   }
+
+// !end
+
+//todo task 2
+
+// let cem = 0;
+// let a = +prompt("eded daxil et");
+// while (a >= 0) {
+//   cem += a;
+//   let a = +prompt("eded daxil et");
+// }
+
+// console.log(cem);
+
+// todo  end alinmadi
+
+//todo  task-3 alinmadi
+// let a = +prompt("eded daxil et");
+// do {
+//   console.log(a);
+// } while (a < 1 || a > 10);
+
+//todo task 3 end
+
+// ? Task-4 start
+// for (let index = 1; index <= 100; index += 5) {
+//   console.log(index);
+// }
+
+// ! task-4 end
+
+// ? Task-5 start
+
+// todo while ile
+
+// let faktorial = 1;
+// for (let index = 1; index <= 10; index++) {
+//   faktorial *= index;
+// }
+
+// console.log(faktorial);
+
+//todo  for ile
+
+// let a = 1;
+
+// while (a <= 100) {
+//   console.log(a);
+//   a += 5;
+// }
+
+//! task-5 end
+
+//? task-6
+// let cem = 0;
+// for (let index = 1; index <= 100; index++) {
+//   if (index % 3 === 0 && index % 6 === 0) {
+//     cem += index;
+//   }
+// }
+
+// console.log(cem);
+
+// ! task 6 end
+
+// ? Task-7 start
+// let hasil = 1;
+// for (let index = 50; index <= 70; index++) {
+//   if (index % 3 === 0) {
+//     hasil *= index;
+//   }
+// }
+
+// console.log(hasil);
+
+// ! task-7 end
+
+// ? Task-8 start
+
+// let count = 1;
+// for (let index = -10; index <= 30; index++) {
+//   if (index % 2 === 0) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+// ! task-8 end
